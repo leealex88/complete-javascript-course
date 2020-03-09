cc36b850328aee209bf82a70f5ff02b8e6d7c4f4
ola <olasvoice@gmail.com>
2020-03-09 14:44:13 +0000

    done
    d

--- 6-budgety/starter/swapi.js
+++ 6-budgety/starter/swapi.js
@@ -1,7 +1,19 @@
-async function callApi() {
-    const person = await fetch('https://swapi.co/api/people/1/').then((res) => res.json());
-    console.log(person);
-    console.log(1 + 1)
-}
+// async function callApi() {
+//     const person = await fetch('https://swapi.co/api/people/1/').then((res) => res.json());
+//     console.log(person);
+//     console.log(1 + 1)
+// }
 
-callApi();
+// callApi();
+
+var str = "daniel"
+
+function capitalise(str) {
+
+    var splitedString = str.split("")
+    console.log(str)
+    for (let i = 0; i < splitedString.length; i++) {
+        var toUpper = splitedString[0].toUpperCase()
+        console.log(toUpper)
+    }
+}
